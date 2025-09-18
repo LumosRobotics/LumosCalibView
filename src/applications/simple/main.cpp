a4cc24ddb37ee483476e858e0d0a27db42f62c32
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2025-09-18 05:56:24 +0200

    Initial commit

---
+++ src/applications/simple/main.cpp
@@ -0,0 +1,36 @@
+#include <QApplication>
+#include <QMainWindow>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QListWidget>
+#include <QLabel>
+#include <QSplitter>
+#include <QTimer>
+#include <QFontDatabase>
+#include <QDir>
+#include <QDebug>
+#include <QWindow>
+#include <QPushButton>
+#include <QMouseEvent>
+#include <QFrame>
+#include <QMenuBar>
+#include <QDialog>
+#include <QRadioButton>
+#include <QButtonGroup>
+#include <QAction>
+#include <QLabel>
+#include <QKeyEvent>
+
+#include <vector>
+#include <string>
+#include <random>
+#include <sstream>
+#include <memory>
+#include <signal.h>
+#include <iostream>
+
+int main()
+{
+}
